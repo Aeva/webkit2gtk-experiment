03e5e32f58f8c01b2e579024c62cc3f58fce1472
Aeva Palecek <aeva@lunarfracking.com>
2016-03-25 14:28:27 -0500

    minor change to build script

--- build.sh
+++ build.sh
@@ -1,3 +1,4 @@
-rm modules/*
+rm -rf modules
+mkdir modules
 g++ -std=c++11 module.cpp -shared -fPIC -o modules/module.so `pkg-config webkit2gtk-web-extension-4.0 --libs --cflags`
 g++ -std=c++11 test.cpp `pkg-config gtkmm-3.0 --libs --cflags` `pkg-config webkit2gtk-4.0 --libs --cflags`
